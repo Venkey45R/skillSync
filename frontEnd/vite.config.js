72ed589c00a01920ba3965450e489051460af7a4
venkey45R <venkateshamulraj@gmail.com>
2025-07-28 18:14:27 +0530

    modified vite configurations

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Add build configuration to externalize @react-oauth/google
+  build: {
+    rollupOptions: {
+      external: ["@react-oauth/google"],
+    },
+  },
+});
