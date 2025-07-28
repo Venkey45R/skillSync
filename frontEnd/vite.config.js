11af9a305a1472e0848151d87ed2821674b527e2
venkey45R <venkateshamulraj@gmail.com>
2025-07-28 18:16:32 +0530

    modified vite configurations for lucide react

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Add build configuration to externalize @react-oauth/google
+  // Add build configuration to externalize @react-oauth/google and lucide-react
   build: {
     rollupOptions: {
-      external: ["@react-oauth/google"],
+      external: ["@react-oauth/google", "lucide-react"], // Added 'lucide-react' here
     },
   },
 });
